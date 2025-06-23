a5b63ef45f59f623bb45236879894e3dbe9ba83e
VinegarChicken <miloaugusteiiioutlook.com>
2025-06-23 11:21:34 -0400

    Icon

---
+++ build.rs
@@ -0,0 +1,11 @@
+#[cfg(windows)]
+fn main() {
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("TasKat.ico");
+    res.compile().unwrap();
+}
+
+#[cfg(not(windows))]
+fn main() {
+    // Do nothing on non-Windows platforms
+}
